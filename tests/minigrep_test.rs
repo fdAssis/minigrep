c2039de9fd49e41250528344d6e8c38f5fac0fef
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-07-27 22:33:06 -0300

    test: returns panic error when parsed enough arguments

--- tests/minigrep_test.rs
+++ tests/minigrep_test.rs
@@ -7,3 +7,14 @@ fn search_line_with_string_parsed() {
 
     assert_eq!(vec!["safe, fast, productive."], search(query, contents));
 }
+
+#[test]
+#[should_panic(expected = "Not enough arguments")]
+fn err_not_enough_arguments() {
+    let args: Vec<String> = vec![];
+    let config = Config::new(&args);
+
+    run(config.unwrap())
+        .map_err(|error| error.to_string())
+        .unwrap_err();
+}
